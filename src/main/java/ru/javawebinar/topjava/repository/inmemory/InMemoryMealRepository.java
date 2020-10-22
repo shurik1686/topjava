3f1237a8fd094efe02bb2bfb905777484371e6ab
shurik16@ya.ru <shurik16@ya.ru>
2020-10-22 23:33:25 +1000

    HW02

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,46 +1,61 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
-import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
+@Repository
 public class InMemoryMealRepository implements MealRepository {
-    private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private final Map<Integer, List<Meal>> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
     {
-        MealsUtil.meals.forEach(this::save);
+        MealsUtil.meals.forEach(meal -> save(meal, 1));
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(Meal meal, int userId) {
+        if (!repository.containsKey(userId))
+            repository.put(1, new LinkedList<>());
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
-            repository.put(meal.getId(), meal);
+            repository.get(userId).add(meal);
             return meal;
         }
         // handle case: update, but not present in storage
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        int index = repository.get(userId)
+                .indexOf(repository.get(userId)
+                        .stream()
+                        .filter(meal1 -> meal.getId().equals(meal1.getId()))
+                        .findFirst().get());
+        return repository.get(userId).set(index, meal);
     }
 
     @Override
-    public boolean delete(int id) {
-        return repository.remove(id) != null;
+    public boolean delete(int id, int userId) {
+        return repository.get(userId).removeIf(meal -> meal.getId() == id);
     }
 
     @Override
-    public Meal get(int id) {
-        return repository.get(id);
+    public Meal get(int id, int userId) {
+        if (getAll(userId).stream().anyMatch(meal -> meal.getId() == id))
+            return getAll(userId).stream().filter(meal -> meal.getId() == id).findFirst().get();
+        else return null;
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        return repository.values();
+    public List<Meal> getAll(int userId) {
+        return repository.get(userId).stream()
+                .sorted((o1, o2) -> o2.getDate().compareTo(o1.getDate()))
+                .collect(Collectors.toList());
     }
 }
 
--- src/main/webapp/users.jsp
+++ src/main/webapp/users.jsp
@@ -1,11 +1,43 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="fn" uri="http://topjava.javawebinar.ru/functions" %>
 <html lang="ru">
 <head>
     <title>Users</title>
 </head>
 <body>
-<h3><a href="index.html">Home</a></h3>
-<hr>
-<h2>Users</h2>
+<section>
+    <h3><a href="index.html">Home</a></h3>
+    <hr>
+    <h2>Users</h2>
+    <a href="users?action=create">Add Meal</a>
+    <br><br>
+    <table border="1" cellpadding="8" cellspacing="0">
+        <thead>
+        <tr>
+            <th>Имя</th>
+            <th>Почта</th>
+            <th>Роли</th>
+            <th>Активный</th>
+            <th>Зарегистрирован</th>
+            <th></th>
+            <th></th>
+        </tr>
+        </thead>
+        <c:forEach items="${users}" var="user">
+            <jsp:useBean id="user" type="ru.javawebinar.topjava.model.User"/>
+            <tr>
+                <td>${user.name}</td>
+                <td>${user.email}</td>
+                <td>${user.roles}</td>
+                <td>${user.enabled}</td>
+                <td>${user.registered.toLocaleString()}</td>
+                <td><a href="users?action=update&id=${user.id}">Update</a></td>
+                <td><a href="users?action=delete&id=${user.id}">Delete</a></td>
+            </tr>
+        </c:forEach>
+    </table>
+</section>
 </body>
 </html>
