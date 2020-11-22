35befbffb9d4f989d74958ce0d8b57a53bd69386
shurik16@ya.ru <shurik16@ya.ru>
2020-11-22 23:02:23 +1000

    lesson06 HW05

--- src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -1,17 +1,21 @@
 package ru.javawebinar.topjava.service;
 
-import org.junit.ClassRule;
+import org.junit.AfterClass;
 import org.junit.Rule;
-import org.junit.rules.ExternalResource;
 import org.junit.rules.Stopwatch;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.ActiveDbProfileResolver;
-import ru.javawebinar.topjava.TimingRules;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 @ContextConfiguration({
         "classpath:spring/spring-app.xml",
@@ -21,9 +25,29 @@ import ru.javawebinar.topjava.TimingRules;
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 @ActiveProfiles(resolver = ActiveDbProfileResolver.class)
 abstract public class AbstractServiceTest {
-    @ClassRule
-    public static ExternalResource summary = TimingRules.SUMMARY;
+    private static final Logger log = getLogger("result");
+
+    private static final StringBuilder results = new StringBuilder();
 
     @Rule
-    public Stopwatch stopwatch = TimingRules.STOPWATCH;
+    // http://stackoverflow.com/questions/14892125/what-is-the-best-practice-to-determine-the-execution-time-of-the-bussiness-relev
+    public Stopwatch stopwatch = new Stopwatch() {
+        @Override
+        protected void finished(long nanos, Description description) {
+            String result = String.format("%-95s %7d", description.getDisplayName(), TimeUnit.NANOSECONDS.toMillis(nanos));
+            results.append(result).append('\n');
+            log.info(result + " ms\n");
+        }
+    };
+
+    //    https://dzone.com/articles/applying-new-jdk-11-string-methods
+    private static final String DELIM = "-".repeat(103);
+
+    @AfterClass
+    public static void printResult() {
+        log.info("\n" + DELIM +
+                "\nTest                                                                                       Duration, ms" +
+                "\n" + DELIM + "\n" + results + DELIM + "\n");
+        results.setLength(0);
+    }
 }
